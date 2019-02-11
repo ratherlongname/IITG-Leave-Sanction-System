178e8f2c888b2257082b922cc4ec4ba2027da230
ratherlongname <shivam.bansal@live.com>
2019-02-12 03:20:59 +0530

    Fixed lots of bugs

--- Login/Form5.Designer.vb
+++ Login/Form5.Designer.vb
@@ -64,7 +64,9 @@ Partial Class Form5
         '
         Me.lsviewDetailLeaveView.BackColor = System.Drawing.Color.White
         Me.lsviewDetailLeaveView.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.Update_ID, Me.Date_Time, Me.User_Action, Me.Action, Me.Remark})
+        Me.lsviewDetailLeaveView.FullRowSelect = True
         Me.lsviewDetailLeaveView.Location = New System.Drawing.Point(228, 294)
+        Me.lsviewDetailLeaveView.MultiSelect = False
         Me.lsviewDetailLeaveView.Name = "lsviewDetailLeaveView"
         Me.lsviewDetailLeaveView.Size = New System.Drawing.Size(824, 358)
         Me.lsviewDetailLeaveView.TabIndex = 0
@@ -323,7 +325,7 @@ Partial Class Form5
         Me.Label7.ForeColor = System.Drawing.Color.White
         Me.Label7.Location = New System.Drawing.Point(47, 11)
         Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(125, 19)
+        Me.Label7.Size = New System.Drawing.Size(124, 19)
         Me.Label7.TabIndex = 12
         Me.Label7.Text = "Appliation Details"
         Me.Label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
